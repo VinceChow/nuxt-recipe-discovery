a15299f8244c04a4bed6c2545b8edd431d067315
Vincent Chow <vincent@gromm.co>
2020-03-22 19:00:25 +0800

    Fix prettier error

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,14 +1,8 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <h1 v-if="error.statusCode === 404">{{ pageNotFound }}</h1>
+    <h1 v-else>{{ otherError }}</h1>
+    <NuxtLink to="/">Home page</NuxtLink>
   </v-app>
 </template>
 
@@ -25,16 +19,16 @@ export default {
     return {
       pageNotFound: '404 Not Found',
       otherError: 'An error occurred'
-    }
+    };
   },
   head() {
     const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError;
     return {
       title
-    }
+    };
   }
-}
+};
 </script>
 
 <style scoped>
