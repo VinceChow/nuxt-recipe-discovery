ae1a80c8456b480ff5b506e7c8315b5ce0816e08
Vincent Chow <vincent@gromm.co>
2020-03-22 21:06:15 +0800

    Remove vue plugin from eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,14 +10,9 @@ module.exports = {
     'prettier/vue',
     'eslint:recommended',
     'plugin:prettier/recommended',
-    'plugin:vue/recommended',
     'plugin:nuxt/recommended'
   ],
   plugins: ['vue', 'nuxt'],
   // add your custom rules here
-  rules: {
-    'no-console': 'off',
-    'vue/max-attributes-per-line': 'off',
-    'vue/singleline-html-element-content-newline': 'off'
-  }
+  rules: {}
 };
